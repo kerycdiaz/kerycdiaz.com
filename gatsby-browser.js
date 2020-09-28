a3ff32b4284bc098e233635b3ff3f087fe8e1b05
Keryc Diaz <kerycdiaz@gmail.com>
2020-09-27 21:19:06 -0500

    update version app

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -9,6 +9,6 @@ export const wrapRootElement = ({ element }) => (
   </ReduxWrapper>
 )
 
-exports.onServiceWorkerUpdateReady = () => {
+export const onServiceWorkerUpdateReady = () => {
   window.location.reload()
 }
