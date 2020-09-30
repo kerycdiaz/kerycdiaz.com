f8952cec072702c21b5a328b026039d7c58d47d2
Keryc Diaz <kerycdiaz@gmail.com>
2020-09-30 03:32:13 -0500

    fix end

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,4 @@
+import NoSsr from '@material-ui/core/NoSsr';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
@@ -6,10 +7,12 @@ import { default as GlobalStyle } from '@styles/globals'
 import theme from '@styles/theme'
 
 export default ({ children }) => (
-  <MuiThemeProvider theme={theme}>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      {children}
-    </ThemeProvider>
-  </MuiThemeProvider>
+  <NoSsr>
+    <MuiThemeProvider theme={theme}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        {children}
+      </ThemeProvider>
+    </MuiThemeProvider>
+  </NoSsr>
 )
