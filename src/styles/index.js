592a136df08aa5afc36f783e0a93d72d69639c72
Keryc Diaz <kerycdiaz@gmail.com>
2020-10-04 20:46:44 -0500

    spee test

--- src/styles/index.js
+++ src/styles/index.js
@@ -2,13 +2,12 @@ import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles'
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
-import { default as GlobalStyle } from '@styles/globals'
+
 import theme from '@styles/theme'
 
 export default ({ children }) => (
   <MuiThemeProvider theme={theme}>
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
       {children}
     </ThemeProvider>
   </MuiThemeProvider>
